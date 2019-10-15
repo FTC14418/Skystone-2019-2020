c9f86f316fbfae05ab6bd2b36677266d09aa14fc
FTC14418 <ftc14418@gmail.com>
2019-10-15 19:52:02 -0400

    teleop
    cleaned up code

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Teleop.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Teleop.java
@@ -26,41 +26,14 @@ public class Teleop extends OpMode {
 
     @Override
     public void loop() {
-       mecanumDrive(gamepad1.right_stick_x, -gamepad1.left_stick_y, gamepad1.left_stick_x);
-
-//        if (Math.abs(gamepad1.left_stick_y) < 0.1 && Math.abs(gamepad1.right_stick_y) < 0.1 && gamepad1.left_bumper)
-//        {
-//            frontLeftMotor.setPower(-1);
-//            frontRightMotor.setPower(-1);
-//            backLeftMotor.setPower(1);
-//            backRightMotor.setPower(1);
-//            // Strafe
-//        }
-//        else if  (Math.abs(gamepad1.left_stick_y) < 0.1 && Math.abs(gamepad1.right_stick_y) < 0.1 && gamepad1.right_bumper){
-//            // Strafe the other way
-//
-//            frontLeftMotor.setPower(1);
-//            frontRightMotor.setPower(1);
-//            backLeftMotor.setPower(-1);
-//            backRightMotor.setPower(-1);
-//        }
-//
-//        else{
-//            frontLeftMotor.setPower(-gamepad1.left_stick_y); // normal driving
-//            frontRightMotor.setPower(-gamepad1.right_stick_y);
-//            backLeftMotor.setPower(-gamepad1.left_stick_y);
-//            backRightMotor.setPower(-gamepad1.right_stick_y);
-//
-//        }
-
+       mecanumDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
     }
 
-
     private void mecanumDrive(double leftx, double lefty, double rightx) {
-        double fl = lefty+leftx+rightx;
-        double fr = lefty-leftx-rightx;
-        double bl = lefty+leftx-rightx;
-        double br = lefty-leftx+rightx;
+        double fl = lefty-leftx+rightx;
+        double fr = lefty+leftx-rightx;
+        double bl = lefty+leftx+rightx;
+        double br = lefty-leftx-rightx;
         frontLeftMotor.setPower(fl);
         frontRightMotor.setPower(fr);
         backLeftMotor.setPower(bl);
