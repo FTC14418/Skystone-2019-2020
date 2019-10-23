098f0968d2edba5cfbf95e90570fb5a04f30da0f
FTC14418 <ftc14418@gmail.com>
2019-10-22 20:03:44 -0400

    Update Teleoptest1.java
    Added arms that open and shut

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Teleoptest1.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Teleoptest1.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="Cooler Design", group="Teleop")
 public class Teleoptest1 extends OpMode {
@@ -12,8 +13,8 @@ public class Teleoptest1 extends OpMode {
     private DcMotor frontRightMotor;
     private DcMotor backLeftMotor;
     private DcMotor backRightMotor;
-    private DcMotor leftArm;
-    private DcMotor rightArm;
+    private Servo leftArm;
+    private Servo rightArm;
 
     @Override
     public void init() {
@@ -21,19 +22,18 @@ public class Teleoptest1 extends OpMode {
         frontRightMotor = hardwareMap.dcMotor.get(Properties.FRONT_RIGHT_MOTOR);
         backLeftMotor = hardwareMap.dcMotor.get(Properties.BACK_LEFT_MOTOR);
         backRightMotor = hardwareMap.dcMotor.get(Properties.BACK_RIGHT_MOTOR);
-        leftArm = hardwareMap.dcMotor.get(Properties.LEFT_ARM);
-        rightArm = hardwareMap.dcMotor.get(Properties.RIGHT_ARM);
+        leftArm = hardwareMap.servo.get(Properties.LEFT_ARM);
+        rightArm = hardwareMap.servo.get(Properties.RIGHT_ARM);
 
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftArm.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
 
         mecanumDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
-        arm(gamepad1.left_trigger, gamepad1.right_trigger);
+        arm(gamepad1.left_trigger);
     }
 
     private void mecanumDrive(double leftx, double lefty, double rightx) {
@@ -46,20 +46,19 @@ public class Teleoptest1 extends OpMode {
         backLeftMotor.setPower(bl);
         backRightMotor.setPower(br);
     }
-    private void arm(double leftt, double rightt) {
-        double armSpeed = 0;
-        if (leftt != 0) {
-            armSpeed = leftt;
-        }
-        else if (rightt != 0) {
-            armSpeed = -rightt;
+    private void arm(double position) {
+        double leftArmPosition = 0;
+        double rightArmPosition = 0;
+        if (position != 0) {
+            leftArmPosition = 1.0;
+            rightArmPosition = 0;
         }
         else {
-            armSpeed = 0;
+            leftArmPosition = 0;
+            rightArmPosition = 1.0;
         }
-
-        leftArm.setPower(armSpeed);
-        rightArm.setPower(armSpeed);
+        leftArm.setPosition(leftArmPosition);
+        rightArm.setPosition(rightArmPosition);
     }
 
 }
