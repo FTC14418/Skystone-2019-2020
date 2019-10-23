41723d29f46c333804021dffe056d08fe4815dab
FTC14418 <ftc14418@gmail.com>
2019-10-22 20:03:17 -0400

    Properties
    Cleaned properties code

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Properties.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Properties.java
@@ -6,13 +6,12 @@ public class Properties {
     public static final String FRONT_RIGHT_MOTOR = "frontRightMotor";
     public static final String BACK_LEFT_MOTOR = "backLeftMotor";
     public static final String BACK_RIGHT_MOTOR = "backRightMotor";
+    public static final String LEFT_ARM = "leftArm";
+    public static final String RIGHT_ARM = "rightArm";
 
     public static final int COUNTS_PER_REV = 0;
 
     public static final double WHEEL_DIAMETER = 90 / 25.4;
 
     public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
-
-    public static final String LEFT_ARM = "leftArm";
-    public static final String RIGHT_ARM = "rightArm";
 }
